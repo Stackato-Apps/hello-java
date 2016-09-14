15d5452141e4320cc5e15965455a50cdcc5ea3a2
Billy Tat <billy.tat@hpe.com>
2016-09-14 14:50:56 -0700

    Update for Stackato 4.0

--- src/main/java/org/cloudfoundry/samples/HelloServlet.java
+++ src/main/java/org/cloudfoundry/samples/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
 
                 // This is only needed because we want to print the IP
 		String ssh_client_info = System.getenv("SSH_CONNECTION");
-		String ip_addr = System.getenv("VCAP_APP_HOST");
+		String ip_addr = "0.0.0.0";
 		if (ssh_client_info != null && ip_addr.equals("0.0.0.0")) {
 			int hubEnd = ssh_client_info.indexOf(" ");
 			int portEnd = ssh_client_info.indexOf(" ", hubEnd + 1);
@@ -27,7 +27,7 @@ public class HelloServlet extends HttpServlet {
 			ip_addr = ssh_client_info.substring(portEnd + 1, dockerIPAddressEnd);
 		}
 
-		writer.println("Hello from " + ip_addr + ":" + System.getenv("VCAP_APP_PORT"));
+		writer.println("Hello from " + ip_addr + ":" + System.getenv("PORT"));
 		writer.close();
 	}
 }
